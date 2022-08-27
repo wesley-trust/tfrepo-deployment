d5cb68c861f30bd91581019bdd7c58149ea26d57
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-27 13:40:33 +0100

    Refactor

---
+++ resources_root.tf
@@ -0,0 +1,39 @@
+# Define resources
+resource "github_repository" "root" {
+  for_each    = toset(var.resource_repository.root)
+  name        = each.value
+  description = var.repository_description[each.value]
+
+  visibility       = var.repository_visibility[each.value]
+  license_template = var.license_template
+
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+  is_template            = true
+
+  template {
+    owner      = var.repository_template_owner
+    repository = var.repository_template[each.value]
+  }
+}
+
+resource "github_branch_protection" "root" {
+  for_each      = toset(var.resource_repository.root)
+  repository_id = github_repository.root[each.value].node_id
+
+  pattern = var.repository_branch_protection
+
+  enforce_admins      = true
+  allows_deletions    = false
+  allows_force_pushes = false
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    restrict_dismissals             = true
+    required_approving_review_count = 0
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,62 @@
-# Define variables
+# Variables
+variable "resource_repository" {
+  description = "The repositories to deploy"
+  type        = map(any)
+  default = {
+    root = [
+      "tfrepo-test"
+    ]
+
+    module = [
+      ""
+    ]
+
+    submodule = [
+      ""
+    ]
+  }
+}
+
+variable "repository_description" {
+  description = "The repository description"
+  type        = map(any)
+  default = {
+    "tfrepo-test" = "Test description"
+  }
+}
+
+variable "repository_visibility" {
+  description = "The visibility of the repository"
+  type        = map(any)
+  default = {
+    "tfrepo-test" = "public"
+  }
+}
+
+variable "repository_template_owner" {
+  description = "The licence template owner"
+  type        = string
+  default     = "wesley-trust"
+}
+
+variable "repository_branch_protection" {
+  description = "The repository branch to protect"
+  type        = string
+  default     = "main"
+}
+
+variable "repository_template" {
+  description = "The template for the repository"
+  type        = map(any)
+  default = {
+    root      = "tfroot-template"
+    module    = "tfmodule-template"
+    submodule = "tfmodule-template"
+  }
+}
+
+variable "license_template" {
+  description = "The licence template for the repo"
+  type        = string
+  default     = "mit"
+}
