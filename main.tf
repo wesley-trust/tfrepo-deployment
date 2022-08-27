751e98762d70bbbf032a3f7684f6a609e00211e7
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-27 14:06:38 +0100

    Test move

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@
 terraform {
   required_version = ">= 1.0.3"
 }
+
+# Main reference of all historical resource moves
+moved {
+
+  # Rename of repository
+  from = github_branch_protection.root["tfrepo-test"]
+  to   = github_branch_protection.root["tfroot-test"]
+}
