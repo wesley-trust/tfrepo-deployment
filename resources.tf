049aa8b6c191db144e3b69998286ed963c80e8a6
wesley-trust <wesley-trust@users.noreply.github.com>
2022-08-27 12:43:49 +0100

    Test repo creation

--- resources.tf
+++ resources.tf
@@ -1,1 +1,12 @@
 # Define resources
+resource "github_repository" "test" {
+  name        = "test"
+  description = "test"
+
+  visibility = "public"
+
+  template {
+    owner      = "wesley-trust"
+    repository = "tfroot-template"
+  }
+}
