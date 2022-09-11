4ad94345e1ae0b2a9ddae3192f17c723e78dc1c9
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-11 16:04:27 +0100

    Correct reference

--- resources_pkr.tf
+++ resources_pkr.tf
@@ -1,12 +1,12 @@
 # Define resources
 resource "github_repository" "pkr" {
-  for_each         = toset(var.resource_repository.repo)
+  for_each         = toset(var.resource_repository.pkr)
   name             = "${each.value}${local.service_deployment}"
   license_template = var.license_template
 
   visibility  = var.repository_visibility[each.value] != null ? var.repository_visibility[each.value] : "private"
   description = var.repository_description[each.value] != null ? var.repository_description[each.value] : null
-  topics      = var.repository_topics.repo != null ? var.repository_topics.repo : null
+  topics      = var.repository_topics.pkr != null ? var.repository_topics.pkr : null
 
   allow_auto_merge       = true
   delete_branch_on_merge = true
@@ -17,7 +17,7 @@ resource "github_repository" "pkr" {
 }
 
 resource "github_branch_protection" "pkr" {
-  for_each      = toset(var.resource_repository.repo)
+  for_each      = toset(var.resource_repository.pkr)
   repository_id = github_repository.pkr[each.value].node_id
 
   pattern = var.repository_branch_protection
